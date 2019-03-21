3d944ed71cc33b0a8ae2fd94568c217e76f0b259
Affan Ahmad <send.affan@gmail.com>
2019-03-21 18:14:14 +0530

    Adding Example App

---
+++ app/src/main/java/affan/ahmad/tagsedittextexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package affan.ahmad.tagsedittextexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/* Copyright (C) 2019 Affan Ahmad Fahmi - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
